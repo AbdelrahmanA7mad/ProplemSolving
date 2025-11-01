943105f968ff4a2fca29901da38cefa617c8c77e
AbdelrahmanA7mad <abdelra7man.ahmad@gmail.com>
2025-11-01 15:24:56 +0200

    First Proplem TWO-SUM

---
+++ Two-Sum/C#/TWO_SUM/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] arr = { 2, 7, 11, 15 };
+        int  target = 9;
+
+        int []result = TwoSum(arr, target);
+
+        for (int i = 0; i < result.Length; i++)
+        {
+            Console.WriteLine(result[i]);
+        }
+
+    }
+
+
+public static int[] TwoSum(int[] nums, int target) {
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i+1; j < nums.Length; j++)
+            {
+
+                if (nums[i] + nums[j] == target)
+                {
+                    int[] result = { i, j};
+
+                    return result;
+                }
+
+            }
+
+        }
+        return new int[0];
+        
+    }
+}
---
+++ Two-Sum/Java/TWO-SUM/src/App.java
@@ -0,0 +1,27 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+            //test;
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        
+
+        for (int i=0;i<nums.length;i++){
+        
+            for (int j=i+1;j<nums.length;j++){
+                
+                if (nums[i] + nums[j]  == target){
+                    
+                    int [] result = {i,j};
+                     return result;
+                }
+            
+            }
+        
+        }
+
+        return new int[0];
+    }
+}
